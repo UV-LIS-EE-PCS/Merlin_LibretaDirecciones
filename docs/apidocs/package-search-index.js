eadd97c570b88d6f8dd6c839562eef6539537bdd
¨Rafael <rafaelmerlinprieto@gmail.com>
2024-05-26 19:48:00 -0600

    eliminando docs

--- docs/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.Address"},{"l":"com.example.AddressData"},{"l":"com.example.utilities"}];updateSearchResults();
